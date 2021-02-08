ab7bea03e9d0003c0e0feacbfce356ece2825db2
G-Obyta <67891458+G-Obrien@users.noreply.github.com>
2021-02-08 09:52:36 +0300

    Parent files pushing
    These are the parent files for the project

---
+++ footer.php
@@ -0,0 +1,106 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<footer>
+<!---Footer----->
+<div class="metal-blue">
+        <!---social links STARTS----->
+        <div class="container foot1">
+            <div class="row">
+                <div class="col-md-4 col-sm-12 col-12 text-left">
+                    <a href="index.php"><img src="assets/logo/Presti.png" alt="presti-logo"></a>  
+                </div>
+               <div class="col-md-4 col-12 text-center">
+               <H4 class="my-md-3 site-title">FOLLOW US </H4>
+            <a href=""> <img src="assets/logo/watsapp.png" alt=""></a>
+            <a href=""> <img src="assets/logo/facebook.png" class="wi" alt=""></a>
+            <a href="">   <img src="assets/logo/twit.png" alt=""></a>
+            <a href=""><img src="assets/logo/gmail.png" class="wi" alt=""></a>
+               </div> 
+               <div class="col-md-4 col-12 text-right">
+                   <p class="my-md-4 header-links">
+                       Help Line
+                        <a href="#" class="px-1">&#45973; +256701481186</a>
+                   </p>
+               </div> 
+            </div>
+            <hr class="classic-rule">
+        </div>
+<!---///-social links ENDS----->
+<!----Main footer----->
+<div class="container foot2">
+    <div class="row">
+        <div class="col-md-3 col-sm-12 col-12 text-left">
+        <H2 class="my-md-3 site-title"> 
+            NEW TO PRESTIGE</H2>
+         <P class="greetings">
+        Subscribe to get every updates on our sales..</P> 
+        <input type="text" name="sub-email" placeholder="Enter your email here.." id="subscribe">
+        <button type="submit" class="subscribe-btn">SUBMIT</button>
+            <br><br>
+            <h3 class="site-title">Get Presti App Now On</h3>
+            <a href=""><img src="assets/logo/apple.jpg" alt=""></a>
+            <a href=""><img src="assets/logo/gplay.jpg" alt=""></a>
+        </div>
+
+       <div class="col-md-3 col-12 text-center about">
+        <H2 class="my-md-3 site-title"> 
+            ABOUT PRESTIGE</H2>
+        <p><a href="">About Us</a></p>
+        <p><a href="">Prestige User Account</a></p>
+        <p><a href="">Privacy Policy</a></p>
+       <p> <a href="">Terms & conditions</a></p>
+        <p><a href="">About Products</a></p>
+
+       </div> 
+
+       <div class="col-md-3 col-12 text-right">
+        <H2 class="my-md-3 site-title"> 
+            GET HELP NOW</H2>
+        <p><a href="">Help Center</a></p>
+        <p><a href="">How to buy from Prestige</a></p>
+        <p><a href="">Payment Methods</a></p>
+       <p> <a href="">Return Policy</a></p>
+        <p><a href="">Report a Product &#8594;</a></p>
+       </div> 
+
+       <div class="col-md-3 col-12 text-right-extreme">
+        <H2 class="my-md-3 site-title"> 
+        PRESTIGE NATIONAL</H2>
+        <P>Find Us every region across the Country..</P>
+        <div class="district">
+            <div class="one">
+                <p><a href="">Arua</a></p>
+                <p> <a href="">Gulu</a></p>
+                <p><a href="">Jinja</a></p>
+                <p><a href="">Kampala</a></p>
+            </div> 
+            <div class="two">
+                <p><a href="">Lira</a></p>
+                <p> <a href="">Masaka</a></p>
+                <p><a href="">Mbarara</a></p>
+                <p><a href="">Soroti</a></p>
+            </div>
+        </div>
+    </div> 
+    </div>
+    <hr class="classic-rule2">
+<p class="copy-right">Prestige Building & Construction Hardware (U) Limited @- 2020-2021</p>
+</div>
+</div>
+   
+</footer>
+
+<!-- JS Bootstrap CDN -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" 
+ integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+</body>
+</html>
