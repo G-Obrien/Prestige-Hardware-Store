7c630f0261083a60117fd3a46c673eb91748edf4
AllenMbabazi <50418190+Allenmabazi@users.noreply.github.com>
2021-02-08 11:07:26 +0300

    Front page
    featured cart, contact,shop

---
+++ contact.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Front View Slider</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudfare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/slide.css">
+</head>
+<body>
+    <script src="js/slide.js"></script>
+<div class="slides">
+    <div class="slide active">
+        <div class="slideContainer">
+            <div class="content">
+                <h2>Slide 1: TOKEN -> 04252676566</h2>
+                <p>This is My First slide;</p>
+                <a href="javascript:;">Explore More</a>
+            </div>
+        </div>
+    </div>
+    <!-- Slide-->
+
+    <div class="slide">
+        <div class="slideContainer">
+            <div class="content">
+                <h2>Slide 2</h2>
+                <p>This is My Second slide;</p>
+                <a href="javascript:;">Follwo For More</a>
+            </div>
+        </div>
+    </div>
+    <!-- Slide-->
+
+    <div class="slide">
+        <div class="slideContainer">
+            <div class="content">
+                <h2>Slide 3</h2>
+                <p>This is My Third slide;</p>
+                <a href="javascript:;">Find It Out</a>
+            </div>
+        </div>
+    </div>
+    <!-- Slide-->
+
+    <div class="slide">
+        <div class="slideContainer">
+            <div class="content">
+                <h2>Slide 4</h2>
+                <p>This is My Fourt slide;</p>
+                <a href="javascript:;">Explore More With ZYGASHI</a>
+            </div>
+        </div>
+    </div>
+    <!-- Slide-->
+
+    <div class="slide">
+        <div class="slideContainer">
+            <div class="content">
+                <h2>Slide 5</h2>
+                <p>This is My Fifth slide;</p>
+                <a href="javascript:;">Explore It Details</a>
+            </div>
+        </div>
+    </div>
+    <!-- Slide-->
+    <!-- Adding Control buttons of slides-->
+    <div class="controls">
+        <div class="prev">
+            <i class="fa fa-arrow-left"></i>
+        </div>
+        <div class="next">
+            <i class="fa fa-arrow-right"></i>
+        </div>
+
+    </div>
+    <!--controls end-->
+    <div class="indicatorContainer">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+    <!--//// indicatorontainer-->
+</div>
+<!-- Slides-->
+</body>
+</html>
